04f50b425cb36e123efb563550ca62157ab49583
junaid <junaidp@gmail.com>
2019-02-18 10:45:01 +0500

    dat class

---
+++ src/com/leavemanagement/client/view/Data.java
@@ -0,0 +1,24 @@
+package com.leavemanagement.client.view;
+
+public class Data {
+
+	private float oldValue;
+	private float sum = 0;
+
+	public float getOldValue() {
+		return oldValue;
+	}
+
+	public void setOldValue(float oldValue) {
+		this.oldValue = oldValue;
+	}
+
+	public float getSum() {
+		return sum;
+	}
+
+	public void setSum(float sum) {
+		this.sum = sum;
+	}
+
+}
